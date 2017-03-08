c4f11c0e37755ac10e901beb1b9cd07c34652b36
Michael Chen <Michael Chen>
2017-03-08 09:25:10 -0600

    advances on practice with the calculator

---
+++ I/Garita/JS class/ejercicioCalc/js/script.js
@@ -0,0 +1,59 @@
+function addEventsListeners(element) {
+
+}
+
+function key0Pressed {
+
+}
+function key1Pressed {
+
+}
+function key2Pressed {
+
+}
+function key3Pressed {
+
+}
+function key4Pressed {
+
+}
+function key5Pressed {
+
+}
+function key6Pressed {
+
+}
+function key7Pressed {
+
+}
+function key8Pressed {
+
+}
+function key9Pressed {
+
+}
+function keyCPressed {
+
+}
+function keyCEPressed {
+
+}
+function keySlashPressed {
+
+}
+function keyMinusPressed {
+
+}
+function keyByPressed {
+
+}
+function keyDotPressed {
+
+}
+function keyPlusPressed {
+
+}
+function keyEqualsPressed {
+
+}
+
