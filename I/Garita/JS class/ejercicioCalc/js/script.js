2ea5b05b9b40251e14a5a14eba2ec02ac0f6952c
Michael Chen <Michael Chen>
2017-03-09 18:26:44 -0600

    almost done

--- I/Garita/JS class/ejercicioCalc/js/script.js
+++ I/Garita/JS class/ejercicioCalc/js/script.js
@@ -1,6 +1,10 @@
 
+//VARIABLES
 var previousValue = 0;
 var previousSign = "none";
+var isFirstEntry = true;
+
+
 function getCurrentValue() {
     return document.getElementById("numberDisplay").value;
 }
@@ -8,7 +12,7 @@ function addValue(value) {
     document.getElementById("numberDisplay").value = value;    
 }
 
-function addNumber(number) {
+function appendNumber(number) {
     var currentValue = getCurrentValue();
     var newValue;
     if(currentValue[0] === "0" && currentValue[1] !== ".") {
@@ -16,6 +20,10 @@ function addNumber(number) {
             newValue += currentValue[i];
         (typeof(newValue) === 'undefined') ? newValue = "" : newValue;
         currentValue = newValue;
+    }
+    if(isFirstEntry) {
+        currentValue = "";
+        isFirstEntry = false;
     }
     currentValue += number;
     addValue(currentValue); 
@@ -32,39 +40,71 @@ function addDot() {
     if(!checkForDots(currentValue)) 
         addValue(currentValue + ".");
 }
+function equal() {
+    var result = 0;
+    if(previousSign === "Plus")
+        result = previousValue += parseFloat(getCurrentValue());
+    if(previousSign === "By")
+        result = previousValue *= parseFloat(getCurrentValue());
+    if(previousSign === "Minus")
+        previousValue -= parseFloat(getCurrentValue());
+    if(previousSign === "Divide")
+        previousValue /= parseFloat(getCurrentValue());
+    addValue(result);
+}
 function resolve() {
-    
+    addValue(previousValue);
+
+}
+function setPreviousSign(sign) {
+    if(sign === "Dot" || sign === "CE" || sign === "C" || sign === "Equal")
+        sign = "none";
+    else
+        isFirstEntry = true;
+    previousSign = sign;
+}
+function clear() {
+    previousValue = 0;
+    previousSign = "none";
+    addValue("0");
 }
 
 function addSign(sign) {
     if(sign === "Dot")
         addDot();
     if(sign === "C")
+        clear();
+    if(sign === "CE")
         addValue("0");
-    if(sign === "CE") {
-        previousValue = 0;
-        addValue("0");
-    }
     if(sign === "Plus") {
-        previousValue += parseInt(getCurrentValue());
-        addValue("0");        
+        previousValue += parseFloat(getCurrentValue());
+        addValue(previousValue);
     }
-    if(sign === "By")
-        previousValue *= parseInt(getCurrentValue());
-        addValue("0");
-    if(sign === "Minus")
-        previousValue -= parseInt(getCurrentValue());
-        addValue("0");
-    if(sign === "Divide")
-        previousValue /= parseInt(getCurrentValue());
-        addValue("0");
+    if(sign === "By") {
+        if (previousValue !== 0) {
+            previousValue *= parseFloat(getCurrentValue());
+            addValue(previousValue);
+        }            
+    }
+    if(sign === "Minus") {
+        previousValue -= parseFloat(getCurrentValue());
+        addValue(previousValue);
+    }
+    if(sign === "Divide") {
+        if (previousValue !== 0) {
+            previousValue /= parseFloat(getCurrentValue());
+            addValue(previousValue);
+        }         
+    }
     if(sign === "Equal")
-        var result = 
+        equal();
+
+    setPreviousSign(sign);
 }
 
 function keyPressed(key) {
     if(typeof(key) === 'number')
-        addNumber(key);
+        appendNumber(key);
     else
         addSign(key);
 }
