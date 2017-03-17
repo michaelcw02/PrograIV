df5b40e01606c387de88f387ca16e84844b1856d
Michael lab 1005 <michael.cw02@hotmail.com>
2017-03-17 13:11:53 -0600

    ejercicio resuelto

---
+++ I/Ejercicio VIII JavaScript-Validacion-Propuesto/resuelto/js/Storage.js
@@ -0,0 +1,31 @@
+﻿Storage = {
+store : function (id, object){
+			return localStorage.setItem(id, JSON.stringify(object,this.replacer));
+		},
+
+retrieve: function (id){
+			  var jsonObject = localStorage.getItem(id);
+			  if(jsonObject === null){
+				return null;
+			  }
+			  else{
+				 var javaScriptObject = JSON.parse(jsonObject,this.revive);
+				 return javaScriptObject;
+			  }
+		},
+
+revive: function (k,v) {
+		if (v instanceof Object && v._class == 'Persona') {
+			return new Persona(v.cedula,v.nombre,v.sexo);
+		}
+		return v;
+	},
+
+replacer: function (k,v) {
+		if (v instanceof Persona) {
+			v._class="Persona";
+		}
+		return v;
+	}
+};
+
