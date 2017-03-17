7a9534fb495f7c47df0c9cb6c78607f5c14b2afe
Michael lab 1005 <michael.cw02@hotmail.com>
2017-03-17 14:38:26 -0600

    couldnt finish the exercise

--- I/Ejercicio 08 Json-linked-with-lastExercise/Ejercicio/js/validations.js
+++ I/Ejercicio 08 Json-linked-with-lastExercise/Ejercicio/js/validations.js
@@ -3,19 +3,25 @@
   function pageLoad(event){
     var cedula=document.getElementById("cedula"); 
     var nombre=document.getElementById("nombre");	
-	cedula.addEventListener("focus",doFocus);
-    cedula.addEventListener("blur",doBlur); 
-	nombre.addEventListener("focus",doFocus);
-    nombre.addEventListener("blur",doBlur); 
+		cedula.addEventListener("focus",doFocus);
+		cedula.addEventListener("blur",doBlur); 
+		nombre.addEventListener("focus",doFocus);
+    nombre.addEventListener("blur",doBlur);
+
     var formulario=document.getElementById("formulario");
     formulario.addEventListener("submit",doValidate);
-	var consultar=document.getElementById("consultar");	
-	consultar.addEventListener("click",doQuery);
-	personas = Storage.retrieve("personas");
-    if (personas == null){
-		personas=[];
-		Storage.store("personas",personas);
-	}
+
+		var consultar=document.getElementById("consultar");	
+		consultar.addEventListener("click",doQuery);
+
+		var borrar = document.getElementById("btn-borrar");
+		borrar.addEventListener("click", doErase);
+//checks for stored "personas"
+		personas = Storage.retrieve("personas");
+		if (personas == null){
+			personas=[];
+			Storage.store("personas", personas);
+		}
   }
   function doFocus(event){
     event.target.className="focus";
@@ -63,39 +69,56 @@
     var nombre=document.getElementById("nombre");
     var sexoMasc=document.getElementById("sexoMasc");	
     var sexoFem=document.getElementById("sexoFem");
-	var sexo=(sexoMasc.checked)? "M": "F";
-	persona = new Persona(cedula.value,nombre.value,sexo);
-	personas.push(persona);
-	Storage.store("personas",personas);
-	window.alert("Data sent: "+persona.completo("-"));
+		var sexo=(sexoMasc.checked)? "M": "F";
+		persona = new Persona(cedula.value,nombre.value,sexo);
+		personas.push(persona);
+		Storage.store("personas",personas);
+		window.alert("Data sent: "+persona.completo("-"));
     var formulario=document.getElementById("formulario");
     formulario.reset();	
   }
   function doQuery(){
     var cedula=document.getElementById("cedula");
-	cedula.classList.remove("invalid");
-	persona = personas.find(function(p){return p.cedula==cedula.value; });
+		cedula.classList.remove("invalid");
+		persona = personas.find( (p) => p.cedula == cedula.value );
     if (persona != null){
-		showPersona(persona);
-	}
-	else{
-		cedula.classList.add("invalid");	
-	}
+			showPersona(persona);
+		}
+		else{
+			cedula.classList.add("invalid");	
+		}
   }
- function showPersona(persona){
+	function doErase() {
+		var cedula = document.getElementById("cedula");
+		cedula.classList.remove("invalid");
+		persona = personas.find( (p) => p.cedula == cedula.value );
+		personaIndex = personas.indexOf(persona);
+		var erased = personas.splice(personaIndex, 1);
+		Storage.store("personas", persona);
+		if(erased == "")
+			cedula.classList.add("invalid");
+		else {
+			var formulario = document.getElementById("formulario");
+			window.alert(persona.nombre + " fue borrado exitosamente!");
+			formulario.reset();
+		}
+		
+		
+	}
+ 	function showPersona(persona){
     var cedula=document.getElementById("cedula"); 
     var nombre=document.getElementById("nombre");
     var sexoMasc=document.getElementById("sexoMasc");	
     var sexoFem=document.getElementById("sexoFem");
 	
-	cedula.value = persona.cedula;
-	nombre.value = persona.nombre;
-	if(persona.sexo=="M"){
-	  sexoMasc.checked=true;
-	}
-	else{
-	  sexoFem.checked=true;
-	}
+		cedula.value = persona.cedula;
+		nombre.value = persona.nombre;
+		if(persona.sexo=="M"){
+			sexoMasc.checked=true;
+		}
+		else{
+			sexoFem.checked=true;
+		}
 }  
   
   
