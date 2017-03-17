c6b0723cb9dcd88623c123b858edd22151c31d74
Michael lab 1005 <michael.cw02@hotmail.com>
2017-03-17 13:48:34 -0600

    changes on folders

--- I/Ejercicio 08 Json-linked-with-lastExercise/Ejercicio/js/Storage.js
+++ I/Ejercicio 08 Json-linked-with-lastExercise/Ejercicio/js/Storage.js
@@ -1,6 +1,6 @@
 ﻿Storage = {
 store : function (id, object){
-			return localStorage.setItem(id, JSON.stringify(object,this.replacer));
+			return localStorage.setItem(id, JSON.stringify(object,this.replacer));	//REPLACER IS A FUNCTION THAT JSON DOES BEFORE stringifyING
 		},
 
 retrieve: function (id){
@@ -9,18 +9,19 @@ retrieve: function (id){
 				return null;
 			  }
 			  else{
-				 var javaScriptObject = JSON.parse(jsonObject,this.revive);
+				 var javaScriptObject = JSON.parse(jsonObject,this.revive);	
 				 return javaScriptObject;
 			  }
 		},
 
+//k STANDS FOR key
 revive: function (k,v) {
 		if (v instanceof Object && v._class == 'Persona') {
 			return new Persona(v.cedula,v.nombre,v.sexo);
 		}
 		return v;
 	},
-
+//k STANDS FOR key
 replacer: function (k,v) {
 		if (v instanceof Persona) {
 			v._class="Persona";
